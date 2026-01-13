6c4ac7b96e212880ce6206af1d65945042dd90c6
miguel <miatorresch@unal.edu.co>
2026-01-12 23:41:44 -0500

    menu inicial

--- frontend/src/pages/Productos.jsx
+++ frontend/src/pages/Productos.jsx
@@ -7,6 +7,7 @@ import { Button, Modal } from 'antd';
 import { useProductsHandler } from '../hooks/useProductsHandler';
 import { useNavigate } from 'react-router';
 import { SearchOutlined, CloseOutlined } from '@ant-design/icons';
+import MenuBar from '../components/MenuBar';
 
 
 function Productos() {
@@ -26,44 +27,48 @@ function Productos() {
 
 
     return (
-        <div className="products-page">
-            <div className="products-page-filters-and-actions">
-                <Input placeholder="Buscar" 
+        <div className="page-container">
+            <MenuBar />
+            <div className="products-page">
+                <div className="products-page-filters-and-actions">
+                    <Input placeholder="Buscar"
                         className="products-page-search-input"
                         prefix={<SearchOutlined />}
                         suffix={search ? <CloseOutlined onClick={() => setSearch('')} /> : null}
                         value={search}
                         onChange={(e) => setSearch(e.target.value)}
-                        />
+                    />
 
-                <Button 
-                onClick={() => navigate('/nuevo-producto')}
-                className="create-product-button"
-                >Crear Artículo</Button>
+                    <Button
+                        onClick={() => navigate('/nuevo-producto')}
+                        className="create-product-button"
+                    >Crear Artículo</Button>
+                </div>
+
+
+                <div className="products-table">
+                    <Table
+                        rowSelection={{ type: selectionType, ...rowSelection }}
+                        columns={columns}
+                        dataSource={tableData}
+                        pagination={{ pageSize: 10 }}
+                        onRow={handleRowClick}
+                    />
+                </div>
+                <Modal
+                    title="Fuera de servicio"
+                    closable={false}
+                    open={isModalOpen}
+                    onOk={handleOk}
+                    cancelButtonProps={{ style: { display: 'none' } }}
+                >
+                    <p>Lo sentimos, en este momento el servicio no está disponible</p>
+                    <p>Por Favor intentelo más tarde</p>
+                </Modal>
+
             </div>
-
-
-            <div className="products-table">
-                <Table
-                    rowSelection={{ type: selectionType, ...rowSelection }}
-                    columns={columns}
-                    dataSource={tableData}
-                    pagination={{ pageSize: 10 }}
-                    onRow={handleRowClick}
-                />
-            </div>
-            <Modal
-                title="Fuera de servicio"
-                closable={false}
-                open={isModalOpen}
-                onOk={handleOk}
-                cancelButtonProps={{ style: { display: 'none' } }}
-            >
-                <p>Lo sentimos, en este momento el servicio no está disponible</p>
-                <p>Por Favor intentelo más tarde</p>
-            </Modal>
-
         </div>
+
     );
 }
 
