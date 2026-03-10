693bdf367d6fb316bd176b929a71e22a982efe56
miguel <miatorresch@unal.edu.co>
2026-03-09 21:02:43 -0500

    iniciando pantalla de cobro

--- frontend/src/pages/Cobro.jsx
+++ frontend/src/pages/Cobro.jsx
@@ -1,7 +1,38 @@
- const Cobro = () => {
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useOrder } from '../context/OrderContext';
+import { ButtonClose } from '../components/buttons/ButtonClose';
+import { ButtonSecundary } from '../components/buttons/ButtonSecundary';
+const Cobro = () => {
+
+const navigate = useNavigate();
+const { items } = useOrder();
+
+    useEffect(() => {
+        console.log(items);
+    }, [items]);
+
     return (
-        <div>
-            <h1>Cobro</h1>
+        <div className="cobro__container">
+            <div className="cobro__header">
+                <div>
+
+                    <ButtonClose
+                        onClick={() => navigate(-1)}
+                    />
+                  
+                </div>
+
+                <div>
+                    Venta actual ( {items.length} )
+                </div>
+                <div>
+                    <ButtonSecundary
+                        label="Eliminar artículos"
+                        onClick={() => navigate(-1)}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
