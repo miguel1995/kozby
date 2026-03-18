c2fd5d970a574160401b34ce4ad5104bec61bc46
miguel <miatorresch@unal.edu.co>
2026-03-18 13:20:51 -0500

    agregar icono de cerrar sesion

--- frontend/src/pages/Plus.jsx
+++ frontend/src/pages/Plus.jsx
@@ -2,16 +2,20 @@ import MenuBar from "../components/MenuBar"
 import { PLUS_MENU_ITEMS } from "../utils/constants"
 import { Divider } from "antd"
 import { useNavigate } from "react-router"
-import { UnorderedListOutlined, InboxOutlined, DollarOutlined } from '@ant-design/icons';
+import { UnorderedListOutlined, InboxOutlined, DollarOutlined, LogoutOutlined } from '@ant-design/icons';
 
 export const Plus = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+
+    const icons = {
+        active: <UnorderedListOutlined />,
+        archived: <InboxOutlined />,
+        tx: <DollarOutlined />,
+        logout: <LogoutOutlined />
+    };
 
     const getIcon = (icon) => {
-        if (icon === 'active') return <UnorderedListOutlined />;
-        if (icon === 'archived') return <InboxOutlined />;
-        if (icon === 'tx') return <DollarOutlined />;
-        return null;
+        return icons[icon] || null;
     };
 
     return (
@@ -19,14 +23,17 @@ export const Plus = () => {
             <div className="plus-welcome-text">Te damos la bienvenida de Nuevo</div>
 
             {PLUS_MENU_ITEMS.map((item) => (
+                <>
                 <div
-                    key={item.key || item.path || item.label}
+                    key={item.key}
                     className="plus-menu-item-label"
                     onClick={() => navigate(item.path)}
                 >
                     <span className="plus-menu-item-icon">{getIcon(item.icon)}</span>
                     <span>{item.label}</span>
                 </div>
+                <Divider className="plus-menu-item-divider" />
+                </>
             ))}
         </div>
     )
